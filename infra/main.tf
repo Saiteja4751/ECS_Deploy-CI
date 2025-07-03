6330cc7b0031037eb4fdf9c47335d9f70cf4947f
Saiteja4751 <sai.battu@atyeti.com>
2025-07-03 11:18:30 +0530

    updat emain.tf

--- infra/main.tf
+++ infra/main.tf
@@ -25,6 +25,19 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
+
+lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = true
+    ignore_changes        = [name]
+}
+
+
+
+
+
+
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
