4acf864c8ea27434f670524d1d1904cf3e6bd842
Saiteja4751 <sai.battu@atyeti.com>
2025-07-03 11:11:53 +0530

    addded with the home applicatoin

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deployed via ECS + ECR + GitHub Actions + Terraform!"
+    return "Deployed via ECS + ECR + GitHub Actions + Terraform! with Sai"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
